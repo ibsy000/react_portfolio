0649bd5e0cdd2dc59c2816a00c298c452b87aa2c
Ibsy Matute <ibsy@rocketmail.com>
2023-02-20 17:38:54 -0600

    converting footer and contact me to react components

---
+++ src/components/FooterAndContact.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function FooterAndContact() {
+    return (
+        <>
+            {/*------------------------- Footer -----------------------------*/}
+            <div className="container" id="contact">
+                <div className="row bg-dark justify-content-center align-items-center" id="footer">
+
+                    {/*---------------- Contact Me Header -------------------*/}
+                    <div className="d-flex justify-content-center align-items-center">
+                        <h4 id="contact-header"><code>&lt; <b>Contact Me</b> /&gt;</code></h4>
+                    </div>
+                    <hr id="contact-hr"></hr>
+
+                    {/*--------------------- Social Icons -------------------*/}
+                    
+
+                </div>
+
+            </div>
+        </>
+    )
+}
