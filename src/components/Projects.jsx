67660ea48a787768e9b742e3877c0f7e9f7e9cb4
Ibsy Matute <ibsy@rocketmail.com>
2023-02-20 17:07:58 -0600

    converted projects to react component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Projects() {
+    return (
+        <>
+            {/*----------------------- Projects -----------------------------*/}
+            <hr id="projects"/>
+            <div class="container">
+                <div class="row">
+
+                    {/*-------------------- Projects Header -----------------*/}
+                    <h1 id="projects-header"><code>< Projects /></code></h1>
+                    <hr/>
+
+                    {/*----------------------- Accordian --------------------*/}
+                    <div class="accordion accordion-flush" id="accordionFlushExample">
+
+                        {/*------------------- Project 1 --------------------*/}
+                        
+                        
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
